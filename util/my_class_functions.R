3a59c622e773c7f01bbf7dd44a70d06846dcb6bb
JR <john.rinn@colorado.edu>
2022-08-01 17:03:18 -0600

    code-a-fried

--- util/my_class_functions.R
+++ util/my_class_functions.R
@@ -37,6 +37,35 @@ import_peaks <- function(consensus_file_path = broadpeakfilepath) {
 }
 
 
+#' CREATE CONSENSUS PEAKS
+#' this function will take multiple replicate .broadPeak files (also narrow)
+#' find peaks that overlap in all the replicates. 
+#' @description 
+#' input set of chipseq replicate peak files
+#' this function then creates one merged file peaks in all samples
+#' @param dbp
+#' This will be extracted with names(GR_list) in the lapply at end of fun
+#' You will need a "dbps" or some object for the lapply that has the 
+#' name of each dbp in the named GRanges list
+#' 
+#' @param peak_list
+#' Named list of GRanges for each chipseq replicate
+#' peak_list can be generated using import_peaks function above
+
+consensus_from_reduced <- function(dbp, peak_list) {
+dbp_peaks <- peak_list[grepl(as.character(dbp), names(peak_list))]
+suppressWarnings(all_peaks <- GenomicRanges::reduce(unlist(as(dbp_peaks, "GRangesList"))))
+peak_exists <- matrix(NA, nrow = length(all_peaks), ncol = length(dbp_peaks))
+for(i in 1:length(dbp_peaks)) {
+  suppressWarnings(peak_exists[,i] <- as.numeric(countOverlaps(all_peaks, dbp_peaks[[i]]) > 0))
+}
+
+# filter to consensus requiring peaks to be in all replicates
+dbp_consensus <- all_peaks[rowSums(peak_exists) == ncol(peak_exists)]
+# Required only two replicates == dbp_consensus <- all_peaks[rowSums(peak_exists) > 1]
+return(dbp_consensus)
+}
+
 
 
 
